579a2fa8bc170c54d7f7792fba8e7030a8c3fa28
Stuart Hendren <stuart@committed.io>
2022-10-19 10:20:03 +0100

    Adds timeout to backup
    fixes #15

--- src/main/java/io/committed/gazetteer/BackupController.java
+++ src/main/java/io/committed/gazetteer/BackupController.java
@@ -8,6 +8,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import javax.sql.DataSource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +22,9 @@ public class BackupController {
 
   private final JdbcTemplate template;
 
+  @Value("${gazetter.backup.timeout:300}")
+  private int timeout;
+
   public BackupController(JdbcTemplate template) {
     this.template = template;
   }
@@ -31,6 +35,7 @@ public class BackupController {
   public void createBackup(@RequestBody Backup backup) throws SQLException {
     log.info("Creating backup {}", backup.getFile());
     try (PreparedStatement statement = getConnection().prepareStatement("SCRIPT TO ?")) {
+      statement.setQueryTimeout(timeout);
       statement.setString(1, backup.getFile());
       statement.execute();
     }
@@ -45,6 +50,7 @@ public class BackupController {
     try (PreparedStatement preparedStatement = connection.prepareStatement("DROP ALL OBJECTS;")) {
       preparedStatement.executeUpdate();
       try (PreparedStatement statement = connection.prepareStatement("RUNSCRIPT FROM ?")) {
+        statement.setQueryTimeout(timeout);
         statement.setString(1, backup.getFile());
         statement.execute();
       }
